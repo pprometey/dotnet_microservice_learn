9090d0fc6fe19e06cd77c9545b2386031658f14f
Alexey Chernyavskiy <pprometey@gmail.com>
2022-10-06 00:19:15 +0600

    Add synchronous communication between services over http

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ builder.Services.AddDbContext<PlatformDbContext>(options =>
     options.UseInMemoryDatabase("PlatformDb"));
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -14,6 +16,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddSwaggerGen();
+
+Console.WriteLine($"--> CommandsService Endpoint {builder.Configuration["CommandsService"]}");
 
 var app = builder.Build();
 
